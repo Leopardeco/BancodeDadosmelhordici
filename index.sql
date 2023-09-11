5145708405f725cbb2e6c9c89dce20e6eb3016be
Leopardeco <leeonardoolliveira1@gmail.com>
2023-09-11 20:43:39 -0300

    exercicio19

--- index.sql
+++ index.sql
@@ -182,6 +182,18 @@ LEFT JOIN (
 ) AS livros_vendidos ON livros.id = livros_vendidos.livro_id
 GROUP BY autores.id;
 
+-- exercício 18
+SELECT autores.nome, SUM(livros_vendidos.qtd_vendida * 20) AS receita_total
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id
+LEFT JOIN (
+    SELECT livro_id, COUNT() AS qtd_vendida
+    FROM vendas
+    GROUP BY livro_id
+) AS livros_vendidos ON livros.id = livros_vendidos.livro_id
+GROUP BY autores.id;
+
+
 
 
 
